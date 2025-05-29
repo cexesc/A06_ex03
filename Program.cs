5e37fb87b7263865dd6edd3b55d6b97eb3c26f71
cexesc <cescblesas@.com>
2025-05-29 18:44:12 +0200

    Versió 1

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Metodes
+{
+    public static List<int> comptadorMultiples(int v)
+    {
+        List<int> multiples = new List<int>();
+        for (int i = 0; i < v; i++)
+        {
+            if (i % 7 == 0)
+            {
+                multiples.Add(i);
+            }
+        }
+        return multiples;
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Introdueix un valor: ");
+        int valor = Convert.ToInt32(Console.ReadLine());
+        var multiples = Metodes.comptadorMultiples(valor);
+        Console.WriteLine("Múltiples de 7 menors que " + valor + ":");
+        Console.WriteLine(string.Join(", ", multiples));
+    }
+
+
+    int i = 0;
+}
+
